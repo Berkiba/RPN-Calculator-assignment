61ac699c9a943c51c907e1bb7c7bb33e942d1080
Zaid Yassir Sultan <suza24gp@student.ju.se>
2025-09-23 12:28:53 +0200

    hello sultan

--- RPN Calculator/Program.cs
+++ RPN Calculator/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello zaid, World!");
+            Console.WriteLine("Hello zaid sultan, World!");
         }
     }
 }
