e79ad650aea2b86914b6ec6f5b799f95eb9a5f8c
2jafa <2jafa@DATOR-JAGO>
2025-09-23 12:37:14 +0200

    jag g hdt vc

--- RPN Calculator/Program.cs
+++ RPN Calculator/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello zaid sultan, World!");
+            Console.WriteLine("Hello jafar sultan, World!");
         }
     }
 }
