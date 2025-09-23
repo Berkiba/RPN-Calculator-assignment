027ed2d38c17769bee4578860d69887679abc242
2jafa <2jafa@DATOR-JAGO>
2025-09-23 12:54:23 +0200

    a broro

--- RPN Calculator/Program.cs
+++ RPN Calculator/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello jafar sultan, World!");
+            Console.WriteLine("Hello jalllfar sultan, World!");
         }
     }
 }
